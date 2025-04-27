4fc2cd6496554233e1a9b8f40e6dc0f9b0644148
poovithash <poovithashunmugaraja@gmail.com>
2025-04-27 16:16:11 +0530

    Initial model API

---
+++ model_api.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# Load your trained model
+model = pickle.load(open('model/energy_model.pkl', 'rb'))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    
+    # Example: you expect a list of features
+    features = data[['country', 'year']]
+    
+    prediction = model.predict([features])
+    output = prediction[0]
+    
+    return jsonify({'prediction': output})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
